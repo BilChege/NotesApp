ed30804a3921157f06a4c34cb77a28effb02c7c6
BilChege <bildadchege2@gmail.com>
2022-11-10 18:33:22 +0300

    updates

---
+++ app/src/main/java/com/example/notesapp/NoteRepository.java
@@ -0,0 +1,33 @@
+package com.example.notesapp;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    public NoteDAO noteDAO;
+    private LiveData<List<Note>> allNotes;
+
+    NoteRepository(Application application){
+        NoteDataBase noteDataBase = NoteDataBase.getDatabase(application);
+        noteDAO = noteDataBase.noteDAO();
+        allNotes = noteDAO.getNotes();
+    }
+
+    LiveData<List<Note>> getAllNotes(){
+        return allNotes;
+    }
+
+    void insert(Note note){
+        NoteDataBase.databaseWriteExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                noteDAO.insert(note);
+            }
+        });
+    }
+
+}
